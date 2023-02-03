cfaa05a6675fb088d84567a29bf72671664acf59
AryanMore <aryanmore498@gmail.com>
2023-02-03 18:50:15 +0530

    hiding loggers

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'development' && logger, thunk].filter(Boolean);
+const middleWares = [process.env.NODE_ENV === 'production' && logger, thunk].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
